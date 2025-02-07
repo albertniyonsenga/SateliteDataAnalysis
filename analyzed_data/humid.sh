b6fb1f3cda6c5c0e2748d3aa1de6488d109d452d
Amazing Mkhonta <n.mkhonta@alustudent.com>
2025-02-07 11:26:43 -0800

    extracting eswantini data

--- analyzed_data/humid.sh
+++ analyzed_data/humid.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+# Extract all rows with "Eswatini" from the raw data and save them in the analyzed_data directory
+grep "Eswatini" ../raw_data/satelite_temperature_data.csv > humidity_data_Eswatini.csv
+
+echo "Eswatini data has been extracted and saved to analyzed_data/humidity_data_Eswatini.csv"
+
