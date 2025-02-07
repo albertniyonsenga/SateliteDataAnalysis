e4922a8a1c923621034fa69a145ef1cc41bd9d43
AdolehSamuel <S.Adoleh@alustudent.com>
2025-02-07 13:37:38 -0800

    improve data extraction & analysis

--- analyzed_data/humid.sh
+++ analyzed_data/humid.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Extract all rows with "Eswatini" from the raw data and save them in the analyzed_data directory
-grep "Eswatini" ../raw_data/satelite_temperature_data.csv > humidity_data_Eswatini.csv
+# Using Eswatini as the country of choice for our analyzed data
+country_name="Eswatini"
+
+# Extract all rows with "Eswatini" from the raw data and save them in the analyzed_data directory	
+grep "${country_name}," ../raw_data/satelite_temperature_data.csv | sort -t, -k 4 -nr | uniq | cut -d , -f 1,2,4 > humidity_data_${country_name}.csv
 
 echo "Eswatini data has been extracted and saved to analyzed_data/humidity_data_Eswatini.csv"
-
