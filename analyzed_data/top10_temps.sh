6753cde057b536290824b73645fb312cb1cd11d3
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-02-09 16:40:56 +0200

    Update top10_temps.sh
    We added modern option -n to clarify that it's for lines.

--- analyzed_data/top10_temps.sh
+++ analyzed_data/top10_temps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Data analysis and automation
-tail -n +2 ../raw_data/satelite_temperature_data.csv | sort -t, -k 3 -nr | uniq | head -10 | cut -d, -f 1,2,3 > highest_temp.csv
+tail -n +2 ../raw_data/satelite_temperature_data.csv | sort -t, -k 3 -nr | uniq | head -n 10 | cut -d, -f 1,2,3 > highest_temp.csv
 
 echo "Top 10 highest temperatures have been saved to highest_temp.csv"
